555d5136ba5937f692dc021712387087509af5da
Chernov Vladimir <volodya.black@gmail.com>
2016-11-13 20:56:06 +0200

    First start.

--- tester2/tester2/settings.py
+++ tester2/tester2/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = 'tester2.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': 'localhost',
-        'PORT': '5432',
+#    'default': {
+ #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ #       'NAME': 'test',
+  #      'USER': 'test',
+   #     'PASSWORD': 'test',
+    #    'HOST': 'localhost',
+     #   'PORT': '5432',
+  #  }
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
